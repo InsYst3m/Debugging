f59c8c61089f471cd1b568beb8daed117496fc2d
InsYst3m <godlike.insyst3m@gmail.com>
2020-04-19 19:19:16 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+
+
+            Console.ReadLine();
+            Console.WriteLine("End");
+        }
+    }
+}
